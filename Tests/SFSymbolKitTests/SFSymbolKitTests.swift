ad369fcfec3e41fc13f3cae567a9117488a35af1
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-09-27 22:52:33 -0700

    Updated Symbols

--- Tests/SFSymbolKitTests/SFSymbolKitTests.swift
+++ Tests/SFSymbolKitTests/SFSymbolKitTests.swift
@@ -5,48 +5,27 @@
 //  See LICENSE for license information.
 //
 
-import XCTest
+import Testing
 @testable import SFSymbolKit
 import SwiftUI
 
-final class SFSymbolKitTests: XCTestCase {
-    /// Tests the `decodePList` function to ensure it correctly decodes the plist into an array of symbols.
-    func testDecodePList() {
-        // Given
-        /// The expected number of symbols that should be present in the plist
-        let expected5oneSymbolsCount = 5648 /// SF Symbols 5.1
-        let expected5oneSymbolsCountPlusSwift = 5649 /// SF Symbols 5.1 + "swift"
-        let swiftSymbolCount = 6558 /// SF Symbols 6 + "swift"+ "swiftdata"
-        // When
-        /// The actual array of symbols returned by the `decodePList` function.
+final class SFSymbolKitTests {
+
+    @Test("Decode PList correctly")
+    func testDecodePList() async throws {
+        // The expected number of symbols that should be present in the plist
+        /// expected5oneSymbolsCount = 5648 // SF Symbols 5.1
+        /// expected5oneSymbolsCountPlusSwift = 5649 // SF Symbols 5.1 + "swift"
+        /// expected6SymbolCount = 6558 // SF Symbols 6 + "swift" + "swiftdata"
+        let swiftSymbolCount = 6830 // SF Symbols 6 + "swift" + "swiftdata" + "appclip" + "appclip.fill" + "applescript"
+
+        // The actual array of symbols returned by the `decodePList` function.
         let symbols = decodePList()
 
-        // Then
-        /// Asserts that the symbols array is not empty.
-        XCTAssertFalse(symbols.isEmpty, "The symbols array should not be empty.")
-        /// Asserts that the symbols array contains the expected number of symbols.
-        XCTAssertEqual(
-            symbols.count, swiftSymbolCount,
-            "The symbols array should contain \(expected5oneSymbolsCountPlusSwift) symbols."
-        )
-    }
-
-    func testCustomFontLoading() {
-        // Ensure you use the correct PostScript name of the font
-        let fontName = "SF-Pro"
-        let size: CGFloat = 12
-
-        // Convert SwiftUI Font to CTFont to test its existence
-        let ctFont = CTFontCreateWithName(fontName as CFString, size, nil)
-        XCTAssertNotNil(ctFont, "Custom font should be loaded.")
-    }
-
-    func testAllFonts() {
-        let size: CGFloat = 12
-        fontNames.forEach { fontName in
-            // Convert SwiftUI Font to CTFont to test its existence
-            let ctFont = CTFontCreateWithName(fontName as CFString, size, nil)
-            XCTAssertNotNil(ctFont, "Custom font \(fontName) should be loaded.")
-        }
+        // Asserts that the symbols array is not empty.
+        #expect(!symbols.isEmpty, "The symbols array should not be empty.")
+        // Asserts that the symbols array contains the expected number of symbols.
+        #expect(symbols.count == swiftSymbolCount, "The symbols array should contain \(swiftSymbolCount) symbols.")
     }
 }
+
